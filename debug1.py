d62f1d02a4bbbab0a89608514c71b3c8be570f43
Wahaj Al Obid <whalobid@student.rtc.edu>
2024-10-31 22:23:57 -0700

    r

--- debug1.py
+++ debug1.py
@@ -1,16 +1,14 @@
+#fix the Pyhton bug code
 def lone_sum(a, b, c):
-    if a >= b:
-        return c
-    elif a == c:
-        return b
-    elif b == c:
-        return a
-    elif a == b and a == c and b == c:
-        return 0
-    else:
-        return a+b+c
-
-print("lone_sum of 10, 10, 10 should be 0: " + str(lone_sum(10, 10, 10)))
+    total = 0
+    if a != b and a != c:
+        total += a
+    if b != a and b != c:
+        total += b
+    if c != a and c != b:
+        total += c
+    return total
+print("lone_sum of 10, 10, 10 should be 0: " + str(lone_sum(20, 20, 20)))
 print("lone_sum of 1, 2, 3 should be 6: " + str(lone_sum(1, 2, 3)))
 print("lone_sum of 1, 2, 1 should be 2: " + str(lone_sum(1, 2, 1)))
 print("lone_sum of 4, 5, 6 should be 15: " + str(lone_sum(4, 5, 6)))
